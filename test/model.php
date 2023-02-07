559f03550245f0a08355f705865aa04c277d652d
MrPsCHen <cps_1993@126.com>
2023-02-07 19:28:38 +0800

    优化表前缀优先级

---
+++ test/model.php
@@ -0,0 +1,16 @@
+<?php
+
+use EasyDb\Config\MysqlConfig;
+use EasyDb\Db;
+use EasyDb\Drive\MysqlPdoDrive;
+
+include "../vendor/autoload.php";
+
+$config = MysqlConfig::set('10.0.0.9','origin','root','root');
+$drive  = new MysqlPdoDrive();
+Db::setConfig($config);
+Db::setDrive($drive);
+
+$user = new \Test\user();
+
+var_dump($user->count(true));
---
+++ test/user.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Test;
+class user extends \EasyDb\Model
+{
+    public string $prefix = 'userful_';
+
+
+}
